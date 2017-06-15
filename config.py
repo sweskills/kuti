e95d527f00a4cb68e9672e03e10d49f223a6476d
kantologist <azeezfemi17937@yahoo.com>
2017-06-15 15:33:45 +0100

    directory set up

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'advasgdv6r234b34837834864'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    KUTI_MAIL_SUBJECT_PREFIX = '[KUTI]'
+    KUTI_MAIL_SENDER = 'KUTI admin <kuti@sweskills.com>'
+    KUTI_ADMIN = os.environ.get('KUTI_ADMIN')
+    KUTI_POST_PER_PAGE = 10
+    KUTI_FOLLOWERS_PER_PAGE = 10
+    KUTI_FOLLOWED_PER_PAGE = 10
+    KUTI_COMMENTS_PER_PAGE = 10
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI =\
+        'sqlite:////' + os.path.join(basedir, 'data-dev.sqlite')
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI =\
+        'sqlite:////' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+        'sqlite:////' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
---
+++ manage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+from app import create_app, db
+# from app.models import Role, User, Post
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+# def make_shell_context():
+#     return dict(app=app, db=db, User=User, Role=Role, Post=Post)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+# manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+if __name__ == '__main__':
+    manager.run()
