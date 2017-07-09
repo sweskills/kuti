5a6ba6adffc77fa824123d7c37cec9c55b76871b
kantologist <azeezfemi17937@yahoo.com>
2017-07-09 20:55:55 +0100

    FEATURE: sign, signout and admin dashboard implemented, fixed travis failure

---
+++ app/main/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Email, Length, Regexp, EqualTo
+from ..models import User
+from wtforms import ValidationError
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('keep me logged in')
+    submit = SubmitField('Log In')
