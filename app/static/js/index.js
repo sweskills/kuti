cf7b03cabf418676cec94a0fda0c9462de402eee
Hedreez09 <idrisolawonuola@gmail.com>
2017-07-10 16:14:12 +0100

    update of career profile

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,1 +1,19 @@
-console.log("javascript testing");
+$(document).ready(function(){
+    $(".form-wrapper .button").click(function(){
+        var button = $(this);
+        var currentSection = button.parents(".section");
+        var currentSectionIndex = currentSection.index();
+        var headerSection = $('.steps li').eq(currentSectionIndex);
+        currentSection.removeClass("is-active").next().addClass("is-active");
+        headerSection.removeClass("is-active").next().addClass("is-active");
+        
+        $(".form-wrapper").submit(function(e){
+            e.preventDefault();
+        });
+        
+        if(currentSectionIndex === 3){
+            $(document).find(".form-wrapper.section").first().addClass("is-active");
+            $(document).find(".steps li").first().addClass("is-active");
+        }
+    });
+});
