9df7476cb5575e8bd1e309bf8f8bde0bdbd78532
Ahameed01 <ahameedoye@gmail.com>
2017-07-26 16:12:15 +0100

    Schoolreg

--- app/static/js/index.js
+++
@@ -1,9 +0,0 @@
-// Get the modal
-var modal = document.getElementById('login_modal');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
